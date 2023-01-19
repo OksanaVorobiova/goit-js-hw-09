082a39e35705c2da4dbb3c6b7f2b4b4a6085dee6
OksanaVorobiova <ov172608@gmail.com>
2023-01-19 13:04:38 +0200

    tried to config

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -22,13 +22,17 @@ const options = {
   minuteIncrement: 1,
   onClose: function(selectedDates) {
       console.log(selectedDates[0]);  
+      console.log(selectedDays[0].getTime());
 
-      if(selectedDates[0].getTime() < this.defaultDate.getTime()) {
+      if(selectedDates[0].getTime() < Date.now()) {
         window.alert("Please choose a date in the future");
       }
 
      
       startBtn.disabled = false;
+
+      let ms = selectedDates[0] - Date.now();
+      return ms;
   },
 };
 
@@ -44,4 +48,25 @@ function onStartBtn (e) {
     if (input.value === "") {
         alert('Please choose a date in the future');
     }
+
+    convertMs(ms);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
